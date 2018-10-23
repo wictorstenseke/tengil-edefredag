59ddcdbdf38eb57af728385acdd4273d940c66e2
Wictor Stenseke <wictor.stenseke@knowit.se>
2018-10-23 21:09:23 +0200

    some styles changes in css and js

--- tengil.js
+++ tengil.js
@@ -1,6 +1,6 @@
 window.onload = function() {
 
-    var day =  new Date().getDay();
+    var day = new Date().getDay();
     var questions = [
         "Is it Friday yet?",
         "What are you doing here?"
@@ -12,7 +12,7 @@ window.onload = function() {
     ];
     var subTexts = [
         "It's {0} days til Friday!",
-        "It's Friday! It's fikaday!",
+        "It's Friday! Play some music!",
         "Get out!"
     ];
     var question = document.getElementById("question");
@@ -20,27 +20,27 @@ window.onload = function() {
     var subText = document.getElementById("sub-text");
     var spotifyPlayer = document.getElementById("spotify-player");
 
-    setTimeout(function () {
-        if(day < 5) {
+    setTimeout(function() {
+        if (day < 5) {
             question.innerHTML = questions[0];
             answer.innerHTML = answers[0];
             subText.innerHTML = subTexts[0].replace("{0}", 5 - day);
-    
-        } else if(day === 5) {
+
+        } else if (day === 5) {
             question.innerHTML = questions[0];
             answer.innerHTML = answers[1];
             subText.innerHTML = subTexts[1];
             spotifyPlayer.style.display = "block";
-            
-    
-        } else if(day > 5) {
+
+        } else if (day > 5) {
             question.innerHTML = questions[0];
             answer.innerHTML = answers[2];
+            answer.style.fontSize = "160px";
             subText.innerHTML = subTexts[2];
         }
     }, 1000);
 
-    
+
 };
 
 function flip() {
