426b3126d393618c830a6cf65d6ec4112708cf92
Magnus Lervik (eru246) <magnus.lervik@teliacompany.com>
2018-10-23 08:05:35 +0200

    New Lervik stuff

--- tengil.js
+++ tengil.js
@@ -1,5 +1,39 @@
-if 
-  #lervik opens (tengil.js)
-  there will be (magic)
-else
-  this junk will stand firm
+window.onload = function() {
+
+    var day =  7;//new Date().getDay();
+    var questions = [
+        "Is it Friday yet?",
+        "What are you doing here?"
+    ];
+    var answers = [
+        "NO!",
+        "YES!",
+        "It's a weekend!!"
+    ];
+    var subTexts = [
+        "It's {0} days til Friday!",
+        "It's Friday! It's fikaday!",
+        "Get out!"
+    ];
+    var question = document.getElementById("question");
+    var answer = document.getElementById("answer");
+    var subText = document.getElementById("sub-text");
+
+    setTimeout(function () {
+        if(day < 5) {
+            question.innerHTML = questions[0];
+            answer.innerHTML = answers[0];
+            subText.innerHTML = subTexts[0].replace("{0}", 5 - day);
+    
+        } else if(day === 5) {
+            question.innerHTML = questions[0];
+            answer.innerHTML = answers[1];
+            subText.innerHTML = subTexts[1];
+    
+        } else if(day > 5) {
+            question.innerHTML = questions[0];
+            answer.innerHTML = answers[2];
+            subText.innerHTML = subTexts[2];
+        }
+    }, 1000);
+};
